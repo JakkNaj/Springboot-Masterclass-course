1ca9208aa28159df384640d21e651e0dbe2b2144
Jakub Najman <jakub@najman.cz>
2023-08-18 13:48:09 +0200

    Application.properties and profiles

--- src/main/java/com/kubous/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/kubous/demo/customer/CustomerConfiguration.java
@@ -1,15 +1,30 @@
 package com.kubous.demo.customer;
 
+import com.kubous.demo.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
 
     @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
+
+    @Value("${info.author.name}")
+    private String authorName;
+
+    @Bean
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
+        return args -> {
+            System.out.println("command line runner running");
+            System.out.println(authorName);
+            System.out.println(infoApp);
+        };
+    }
 
     @Bean
     CustomerRepo customerRepo() {
