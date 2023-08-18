de7341649f0462357e19a3770b19f36c2be6fb1b
Jakub Najman <jakub@najman.cz>
2023-08-18 10:39:34 +0200

    logging

--- src/main/java/com/kubous/demo/customer/CustomerService.java
+++ src/main/java/com/kubous/demo/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.kubous.demo.customer;
 
 import com.kubous.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,8 @@ import java.util.List;
 
 @Service
 public class CustomerService { //Business logic for domain we are working with (Customer)
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
     //reference to DAO layer
     private final CustomerRepository customerRepository;
@@ -18,11 +22,17 @@ public class CustomerService { //Business logic for domain we are working with (
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id) {
         return customerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("customer with id " + id + " not found"));
+                .orElseThrow(
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException("customer with id " + id + " not found");
+                            LOGGER.error("error in getCustomer id:{}", id, notFoundException);
+                            return notFoundException;
+                        });
     }
 }
--- src/main/java/com/kubous/demo/exception/ApiExceptionHandler.java
+++ src/main/java/com/kubous/demo/exception/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.kubous.demo.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +12,9 @@ import java.time.ZonedDateTime;
 @ControllerAdvice
 //handles the exceptions
 public class ApiExceptionHandler {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
     //this is used to build a response to our client
     @ExceptionHandler(value = ApiRequestException.class)
     public ResponseEntity<Object> handleApiRequestException(
